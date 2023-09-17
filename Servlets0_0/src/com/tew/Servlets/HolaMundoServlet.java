245dee323f3faaec730fa5f541c6fdfe6d8bc1dc
pugafran <franciscplgi36@gmail.com>
2023-09-17 18:43:38 +0200

    TEW-2324-2-UO272109-2.2-Servlets con sesiones.

--- Servlets0_0/src/com/tew/Servlets/HolaMundoServlet.java
+++ Servlets0_0/src/com/tew/Servlets/HolaMundoServlet.java
@@ -2,6 +2,7 @@ package com.tew.Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Vector;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,24 +16,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HolaMundoServlet")
 public class HolaMundoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HolaMundoServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HolaMundoServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    public void doGet (HttpServletRequest request, HttpServletResponse response)
-    		 throws IOException, ServletException {
-    	
-    		
-    	 /*
-    	 	 
+	public void doGet (HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+
+		/*
     		 System.out.println("AAAAAAAA");
     		 response.setCharacterEncoding("UTF-8");
     		 response.setContentType("text/html");
@@ -42,24 +42,47 @@ public class HolaMundoServlet extends HttpServlet {
     		 out.println("<BODY>");
     		 out.println("Bienvenido a mi primera página web!");
     		 out.println("</BODY></HTML>");
-    		 
-    	*/
-    	 String nombre = (String) request.getParameter("NombreUsuario");
-    	 response.setCharacterEncoding("UTF-8");
-    	 response.setContentType("text/html");
-    	 PrintWriter out = response.getWriter();
-    	 out.println("<HTML>");
-    	 out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
-    	 out.println("<BODY>");
 
-    	 if ( nombre != null ){
-    	 out.println("<br>Hola "+nombre+"<br>");
-    	 }
-    	 out.println("Bienvenido a mi primera página Web!");
-    	 out.println("</BODY></HTML>");
-    		 
-    		}
-			
+		 */
+		
+
+		PrintWriter out = response.getWriter();
+		
+		String nombre = (String) request.getParameter("NombreUsuario");
+
+		Vector listado = (Vector)request.getSession().getAttribute("listado");
+		
+		if (listado == null){
+			listado = new Vector();
+		}
+		
+		if ( nombre != null ){
+			out.println("<br>Hola "+nombre+"<br>");
+			listado.addElement(nombre);
+		}
+		
+		request.getSession().setAttribute("listado",listado);
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+		out.println("<BODY>");
+		out.println("Bienvenido a mi primera página Web!");
+		
+
+
+
+		out.println("<br>");
+		out.println("Contigo, hoy me han visitado:<br>");
+		for ( int i = 0 ; i < listado.size() ; i++ ){
+			out.println("<br>"+(String)listado.elementAt(i));
+		}
+		
+		out.println("<br><br>");
+		out.println("<a href=\"index.html\">Volver</a>");
+		out.println("</BODY></HTML>");
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
